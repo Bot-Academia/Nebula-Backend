7b166a69a6b6aa5a42392a63594fd11088cdbc0e
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2020-10-27 16:58:10 +0530

    added config
    Signed-off-by: Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const { json, urlencoded } = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const connect = require('./utils/db');
+
+export const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+export const start = async () => {
+  try {
+    await connect();
+    app.listen(process.env.PORT, () => {
+      console.log(`REST API on http://localhost:${config.port}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
---
+++ utils/db.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connect = (url = process.env.DATABASE_URL, opts = {}) => mongoose.connect(
+  url,
+  { ...opts, useNewUrlParser: true },
+);
+
+module.exports = connect;
