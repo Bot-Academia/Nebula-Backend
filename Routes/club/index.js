f49b61827a4da1324cb3eea956d8e59d32359352
Vinayak Sharma <vinayaksh42@gmail.com>
2020-11-17 05:34:40 +0530

    controllers
    Signed-off-by: Vinayak Sharma <vinayaksh42@gmail.com>

--- Routes/club/index.js
+++ Routes/club/index.js
@@ -1,66 +1,17 @@
-/* eslint-disable no-console */
 const Router = require('express');
 const model = require('../../models/club');
+const {
+  getAll, deleteOne, getOne, createOne,
+} = require('../../controllers/crud');
 
 const router = Router();
 
 router.route('/')
-  .get(async (req, res) => {
-    try {
-      const docs = await model
-        .find({ })
-        .lean()
-        .exec();
-
-      res.status(200).json({ data: docs });
-    } catch (e) {
-      console.error(e);
-      res.status(400).end();
-    }
-  })
-  .post(async (req, res) => {
-    try {
-      const doc = await model.create({ ...req.body });
-      res.status(201).json({ data: doc });
-    } catch (e) {
-      console.error(e);
-      res.status(404).end();
-    }
-  });
+  .get(getAll(model))
+  .post(createOne(model));
 
 router.route('/:id')
-  .get(async (req, res) => {
-    try {
-      const doc = await model
-        .findOne({ _id: req.params.id })
-        .lean()
-        .exec();
-
-      if (!doc) {
-        return res.status(400).end();
-      }
-
-      res.status(200).json({ data: doc });
-    } catch (e) {
-      console.error(e);
-      res.status(400).end();
-    }
-  })
-  .delete(async (req, res) => {
-    try {
-      const removed = await model.findOneAndRemove({
-        _id: req.params.id,
-      });
-
-      if (!removed) {
-        return res.status(400).end();
-      }
-
-      return res.status(200).json({ data: removed });
-    } catch (e) {
-      console.error(e);
-      res.status(400).end();
-    }
-  });
+  .get(getOne(model))
+  .delete(deleteOne(model));
 
 module.exports = router;
